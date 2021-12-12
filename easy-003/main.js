9e6bafd5de11c89b13751e6704cca4000bdc65a1
Marcos Sierra <marcosjspac@gmail.com>
2021-12-12 01:49:23 -0600

    agregando resolucion a reto facil 3

---
+++ easy-003/main.js
@@ -0,0 +1,39 @@
+const keys = {
+    "(": ")",
+    "{": "}",
+    "[": "]"
+}
+
+const closeKeys = {
+    "}": "{",
+    "]": "[",
+    ")": "("
+}
+
+/**
+ * 
+ * @param {string} inputString 
+ * @returns 
+ */
+function main(inputString){
+    let activeKey = [];
+    let output = true
+    const stringArray = inputString.split("");
+    stringArray.forEach((element) => {
+        if(element in keys){
+            activeKey.push(element);
+        }else if(element in closeKeys){
+            if(closeKeys[element] == activeKey[activeKey.length - 1]){
+                activeKey.pop();
+            } else {
+                output = false;
+            }
+        }
+    });
+    if(output){
+        output = activeKey.length > 0 ? false : output;
+    }
+    return output;
+}
+
+// console.log(main("Esto es una prueba({[]})()[]"))
